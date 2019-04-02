b019a51dad6c2214e7d4e99de5d5b54f1c030e74
DESKTOP-LDJ554V\DANIEL <daniel_washington@hotmail.com>
2019-04-03 00:12:38 +0200

    Implementation method for requirement 4.2.1 and formatter for messages

--- src/es/upm/woa/agent/group2/agents/AgWorld.java
+++ src/es/upm/woa/agent/group2/agents/AgWorld.java
@@ -1,10 +1,12 @@
 package es.upm.woa.agent.group2.agents;
 
 import java.util.ArrayList;
+import java.util.Properties;
 import java.util.Random;
 
 import es.upm.woa.agent.group2.beans.Tribe;
 import es.upm.woa.agent.group2.beans.Unit;
+import es.upm.woa.agent.group2.common.MessageFormatter;
 import es.upm.woa.agent.group2.rules.AgWorldRules;
 import es.upm.woa.ontology.Cell;
 import es.upm.woa.ontology.CreateUnit;
@@ -21,6 +23,7 @@ import jade.content.onto.OntologyException;
 import jade.content.onto.basic.Action;
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
@@ -60,6 +63,7 @@ public class AgWorld extends Agent {
 	private ArrayList<Tribe> tribes;
 	private AgWorldRules worldRules;
 	private Cell[][] map;
+	private Properties properties = new Properties();
 	// -----------------------------------------------------------------
 	// Constructor
 	// -----------------------------------------------------------------
@@ -220,6 +224,112 @@ public class AgWorld extends Agent {
 
 		});
 
+		
+
+	    Behaviour handleMovementRequests = new CyclicBehaviour() {
+	        @Override
+	        public void action() {
+	            // Wait for a units request to move to a new position
+	            ACLMessage msg = receive(
+	                MessageTemplate.and(
+	                    MessageTemplate.MatchProtocol("move"),
+	                    MessageTemplate.MatchPerformative(ACLMessage.REQUEST)
+	                )
+	            );
+
+	            if(msg != null) {
+	                AID unitAID = msg.getSender();
+	                String senderName = (unitAID).getLocalName();
+	                String platformName = getLocalName();
+
+	                try {
+	                    ContentElement movementRequest = getContentManager().extractContent(msg);
+
+	                    if (movementRequest instanceof Action) {
+	                        Action action = (Action) movementRequest;
+	                        Concept concept = action.getAction();
+
+	                        if (concept instanceof Cell) {
+	                        	
+	                        	ACLMessage reply = msg.createReply();
+								reply.setLanguage(codec.getName());
+								reply.setOntology(ontology.getName());
+								
+								
+	                        	Cell requestedPosition = (Cell) concept;
+	                        	Cell currentPosition = null; //TODO: DANIEL findUnitFromAnyTribe(unitAID).getPosition();
+
+	                            //ACLMessage reply;
+
+	                            if (!areAdjacentPositions(currentPosition, requestedPosition)) {
+	                                System.out.println(platformName + "Unit " + senderName + " can't move there...");
+	                                reply = MessageFormatter.createReplyMessage(msg, ACLMessage.REFUSE, "move");
+	                                send(reply);
+	                            } else {
+	                                // Next, we'll have to store the new position for this unit
+	                            	//TODO: DANIEL moveUnitToPosition(unitAID, requestedPosition);
+
+	                                reply = MessageFormatter.createReplyMessage(msg, ACLMessage.AGREE, "move");
+	                                send(reply);
+
+	                                // TODO (MAYBE): I'm not sure, but maybe we should inform all subscribers about this change?
+	                                Cell cell = null; //TODO: DANIEL getCellInfo(requestedPosition);
+
+	                                try {
+	                                    // Send an INFORM with its new position
+	                                    Long gameHour = null; //TODO: DANIEL readFloatProp("game.hour.length").longValue();
+	                                    Long movementDuration = null; //TODO: DANIEL readIntProp("unit.movement.duration").longValue();
+
+	                                    // This should be read from the properties file but the format chosen in there is really weird
+	                                    // and casting the types didn't work as expected...
+	                                    doWait(2000);
+	                                    ACLMessage informMsg = MessageFormatter.createMessage(ACLMessage.INFORM, "move", unitAID);
+	                                    getContentManager().fillContent(informMsg, new Action(unitAID, cell));
+
+	                                    send(informMsg);
+	                                } catch (Codec.CodecException | OntologyException e) {
+	                                    e.printStackTrace();
+	                                }
+
+	                                //try {
+	                                    // Inform all subscribers
+	                                    ArrayList<AID> subscribers = null; //TODO: DANIEL getSubscribers();
+	                                    ArrayList<Tribe> tribes = null; //TODO: DANIEL getTribes();
+
+	                                    for(Tribe tribe : tribes) {
+	                                        ArrayList<Cell> positions = null; //TODO: DANIEL entityManager.retrieveTribeKnownPosition(tribe.getAgentAID());
+
+	                                        // Verify if the tribe knows this position
+	                                        if (positions.contains(requestedPosition)) {
+	                                            // Notify Tribe
+	                                            ACLMessage informMsg = null; //TODO: DANIEL  MessageFormatter.createMessage(ACLMessage.INFORM, "subscribe", tribe.getAgentAID());
+	                                          //TODO: DANIEL getContentManager().fillContent(informMsg, new Action(tribe.getAgentAID(), cell));
+	                                            send(informMsg);
+
+	                                            // Notify Units
+	                                            for (Unit unit : tribe.getUnits()) {
+	                                            	ACLMessage informMsgUnit = null; //TODO: DANIEL MessageFormatter.createMessage(ACLMessage.INFORM, "subscribe", unit.getUnitID());
+	                                              //TODO: DANIEL getContentManager().fillContent(informMsgUnit, new Action(unit.getUnitID(), cell));
+	                                                send(informMsgUnit);
+	                                            }
+	                                        }
+	                                    }
+	                                //}
+	                                //catch (Codec.CodecException | OntologyException e) {
+	                                //    e.printStackTrace();
+	                                //}
+	                            }
+	                        } else { System.out.println("Wrong position."); }
+	                    } else { System.out.println("You lost"); }
+	                } catch (Codec.CodecException | OntologyException e) {
+	                    e.printStackTrace();
+	                }
+	            } else {
+	                // If no message arrives
+	                block();
+	            }
+	        }
+	    };
 	}
 
 	// -----------------------------------------------------------------
@@ -328,4 +438,47 @@ public class AgWorld extends Agent {
 		}
 		return null;
 	}
+
+
+    /**
+     * Checks whether two positions are adjacent to each other.
+     * This is relevant for unit movement, because they can only move to adjacent positions.
+     * TODO: This should definitely be in a separate class...
+     *
+     * @param posA          First position
+     * @param posB          Second position
+     * @return boolean      Whether the two positions are adjacent
+     */
+    private boolean areAdjacentPositions(Cell posA, Cell posB) {
+        // First, both positions need to be valid
+        if(!isValidPosition(posA) || !isValidPosition(posB)) {
+            System.out.println("$$$ This position doesn't exist on our hexagonal map $$$");
+            return false;
+        }
+
+        // Second, the deltas of both dimensions have to be correct
+        int deltaX = Math.abs(posA.getX() - posB.getX());
+        int deltaY = Math.abs(posA.getY() - posB.getY());
+
+        return (deltaX <= 2 && deltaY <= 1);
+    }
+    
+    // TODO: Move to another class
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
+    private boolean isValidPosition(Cell position) {
+        int x = position.getX();
+        int y = position.getY();
+
+        // If the coordinates are outside of map
+        if(x > readIntProp("map.width") || y > readIntProp("map.height")) return false;
+
+        // If the coordinates are negative
+        if(x < 0 || y < 0) return false;
+        return ((x % 2 == 0 && y % 2 == 0) || (x % 2 != 0 && y % 2 != 0));
+    }
+    
+    // TODO: Move to another class
+    private Integer readIntProp(String property) {
+        return Integer.parseInt(properties.getProperty(property));
+    }
 }
---
+++ src/es/upm/woa/agent/group2/common/MessageFormatter.java
@@ -0,0 +1,33 @@
+package es.upm.woa.agent.group2.common;
+
+import es.upm.woa.ontology.GameOntology;
+import jade.content.lang.Codec;
+import jade.content.lang.sl.SLCodec;
+import jade.content.onto.Ontology;
+import jade.core.AID;
+import jade.lang.acl.ACLMessage;
+
+public class MessageFormatter {
+    static private final Codec codec = new SLCodec();
+    static private final Ontology ontology = GameOntology.getInstance();
+
+    public static ACLMessage createMessage(int performative, String protocol, AID receiver) {
+        ACLMessage msg = new ACLMessage(performative);
+        msg.setProtocol(protocol);
+        msg.setLanguage(codec.getName());
+        msg.setOntology(ontology.getName());
+        msg.addReceiver(receiver);
+
+        return msg;
+    }
+
+    public static ACLMessage createReplyMessage(ACLMessage message, int performative, String protocol) {
+        ACLMessage msg = message.createReply();
+        msg.setPerformative(performative);
+        msg.setProtocol(protocol);
+        msg.setLanguage(codec.getName());
+        msg.setOntology(ontology.getName());
+
+        return msg;
+    }
+}
